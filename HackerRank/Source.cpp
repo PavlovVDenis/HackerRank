5d65c70e94764fa9b37b2a82fd048e875b3cd68e
PavlovVDenis <pavlov.v.denis@gmail.com>
2022-12-25 22:57:26 +0400

    Climbing Learedboard(optimized - binary search in vector)

--- HackerRank/Source.cpp
+++ HackerRank/Source.cpp
@@ -465,24 +465,50 @@ int pickingNumbers(vector<int> a) {
  *  2. INTEGER_ARRAY player
  */
 vector<int> climbingLeaderboard(vector<int> ranked, vector<int> player) {
+    vector<pair<int, int>> ranked_pair;
+    int rank = 0;
+    int prevRank = 0;
+    for (auto& r : ranked) {
+        if (r != prevRank) rank++;
+        ranked_pair.push_back(make_pair(r, rank));
+        prevRank = r;
+    }
+    int rankedSize = ranked_pair.size();
+
     vector<int> res;
 
+    for (auto& p : player) {
 
-
-        int rank = 0;
-        int prevRank = 0;
-        if (p > ranked[0]) {
-            res.push_back(1);
-            continue;
+        if (p > ranked_pair[0].first) {
             res.push_back(1);
             continue;
         }
-        for (auto& r: ranked) {
-            if (p < r && r != prevRank) rank++;
-            if (p > r) break;
-            prevRank = r;
+        if (p < ranked_pair[rankedSize - 1].first) {
+            res.push_back(ranked_pair[rankedSize - 1].second + 1);
+            continue;
         }
-        res.push_back(rank + 1);
+        if (p == ranked_pair[rankedSize - 1].first) {
+            res.push_back(ranked_pair[rankedSize - 1].second);
+            continue;
+        }
+
+        bool gotcha = false;
+        int lft = 0;
+        int rgh = rankedSize - 1;
+        int middle;
+        while (lft <= rgh && gotcha != true) {
+            middle = (lft + rgh) / 2;
+            if (ranked_pair[middle].first > p && ranked_pair[middle + 1].first < p) { 
+                gotcha = true; 
+                res.push_back(ranked_pair[middle].second + 1);
+            }
+            if (ranked_pair[middle].first == p && ranked_pair[middle + 1].first < p) { 
+                gotcha = true; 
+                res.push_back(ranked_pair[middle].second);
+            }
+            if (ranked_pair[middle].first >= p) lft = middle + 1;
+            else rgh = middle - 1;
+        }
     }
     return res;
 }
@@ -542,8 +568,19 @@ int main()
     cout << "Longest subarray is " << pickingNumbers(arr) << endl;
     */
 
-    vector<int> ranked = { 100, 90, 90, 80, 75, 60 };
-    vector<int> played = { 50, 65, 77, 90, 102 };
+    vector<int> ranked = { 295, 294, 291, 287, 287, 285, 285, 284, 283, 279, 277, 274, 274, 271, 270, 268, 268, 268, 264, 260, 
+        259, 258, 257, 255, 252, 250, 244, 241, 240, 237, 236, 236, 231, 227, 
+        227, //35 
+        227, 226, 225, 224, 223, 216, 212, 200, 197, 196, 
+        194, 193, 189, 188, 187, 183, 182, 178, 177, 173, 171, 169, 165, 143, 140, 137, 135, 133, 130, 130, 130, 128, 127, 122, 120, 
+        116, 114, 113, 109, 106, 103, 99, 92, 85, 81, 69, 68, 63, 63, 63, 61, 57, 51, 47, 46, 38, 30, 28, 25, 22, 15, 14, 12, 6, 4 };
+    vector<int> played = { 5, 5, 6, 14, 19, 20, 23, 25, 29, 29, 30, 30, 32, 37, 38, 38, 38, 41, 41, 44, 45, 45, 47, 59, 59, 62, 63, 65, 67, 69, 70, 
+        72, 72, 76, 79, 82, 83, 90, 91, 92, 93, 98, 98, 100, 100, 102, 103, 105, 106, 107, 109, 112, 115, 118, 118, 121, 122, 122, 123, 125, 125, 125, 
+        127, 128, 131, 131, 133, 134, 139, 140, 141, 143, 144, 144, 144, 144, 147, 150, 152, 155, 156, 160, 164, 164, 165, 165, 166, 168, 169, 170, 
+        171, 172, 173, 174, 174, 180, 184, 187, 187, 188, 194, 197, 197, 197, 198, 201, 202, 202, 207, 208, 211, 212, 212, 214, 217, 219, 219, 220, 
+        220, 223, 225, 227, 228, 229, 229, 233, 235, 235, 236, 242, 242, 245, 246, 252, 253, 253, 257, 257, 260, 261, 266, 266, 268, 269, 271, 271, 
+        275, 276, 281, 282, 283, 284, 285, 287, 289, 289, 295, 296, 298, 300, 300, 301, 304, 306, 308, 309, 310, 316, 318, 318, 324, 326, 329, 329, 
+        329, 330, 330, 332, 337, 337, 341, 341, 349, 351, 351, 354, 356, 357, 366, 369, 377, 379, 380, 382, 391, 391, 394, 396, 396, 400 };
     auto res = climbingLeaderboard(ranked, played);
     for (auto i : res) cout << i << endl;
 }
