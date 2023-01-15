1fad212aa1af09f7fb61e468f19b81127211fe46
Denis Pavlov <pavlov.v.denis@gmail.com>
2023-01-16 02:18:10 +0700

    Insertion Sort (fix bug)

--- HackerRank/Source.cpp
+++ HackerRank/Source.cpp
@@ -1050,15 +1050,42 @@ void insertionSort2(int n, vector<int> arr) {
     }
 }
 
+// Find the bug!
+//
+void insertionSort(int N, int arr[]) {
+    int i, j;
+    int value;
+    for (i = 1; i < N; i++)
+    {
+        value = arr[i];
+        j = i - 1;
+        // Bug here: while (j >..... 0 && value < arr[j])
+        while (j >= 0 && value < arr[j])
+        {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = value;
+    }
+    for (j = 0; j < N; j++)
+    {
+        printf("%d", arr[j]);
+        printf(" ");
+    }
+}
+
 int main()
 {
     //vector<int> arr = { 2, 4, 6, 8, 3 };
     //vector<int> arr = { 2, 3, 4, 5, 6, 7, 8, 9, 10, 1 };
-    vector<int> arr = { 3, 4, 7, 5, 6, 2, 1 };
+    //vector<int> arr = { 3, 4, 7, 5, 6, 2, 1 };
+    const int arrSize = 6;
+    int arr[arrSize] = { 7, 4, 3, 5, 6, 2 };
     cout << "Entrance ";
     for (auto& a : arr) cout << a << "  ";
     cout << endl;
 
     //insertionSort1(arr.size(), arr);
-    insertionSort2(arr.size(), arr);
+    //insertionSort2(arr.size(), arr);
+    insertionSort(arrSize, arr);
 }
