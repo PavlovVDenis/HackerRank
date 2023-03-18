cda7a375b34770beb433ee96d58837034ab55f96
PavlovVDenis <pavlov.v.denis@gmail.com>
2023-03-18 22:52:01 +0700

    Grading Students

--- HackerRank/Source.cpp
+++ HackerRank/Source.cpp
@@ -1141,14 +1141,38 @@ vector<int> countingSort(vector<int> arr) {
     return res;
 }
 
+/*
+ * Complete the 'gradingStudents' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY grades as parameter.
+ */
+vector<int> gradingStudents(vector<int> grades) {
+    vector<int> res;
+    for (auto g : grades) {
+        int newItem = g;
+        for (int i = 1; i < 3 && g >= 38; i++) {
+            int next5 = newItem + i;
+            if (next5 % 5 == 0) {
+                newItem = next5;
+                break;
+            }
+        }
+        res.push_back(newItem);
+    }
+    return res;
+}
+
+
 int main()
 {
-    vector<int> arr = { 63, 25, 73, 1, 98, 73, 56, 84, 86, 57, 16, 83, 8, 25, 81, 56, 9, 53, 98, 67, 99, 12, 83, 89, 80, 91, 39, 86, 76, 85, 74, 39, 25, 90, 59, 10, 94, 32, 44, 3, 89, 30, 27, 79, 46, 96, 27, 32, 18, 21, 92, 69, 81, 40, 40, 34, 68, 78, 24, 87, 42, 69, 23, 41, 78, 22, 6, 90, 99, 89, 50, 30, 20, 1, 43, 3, 70, 95, 33, 46, 44, 9, 69, 48, 33, 60, 65, 16, 82, 67, 61, 32, 21, 79, 75, 75, 13, 87, 70, 33 };
+    //vector<int> arr = { 63, 25, 73, 1, 98, 73, 56, 84, 86, 57, 16, 83, 8, 25, 81, 56, 9, 53, 98, 67, 99, 12, 83, 89, 80, 91, 39, 86, 76, 85, 74, 39, 25, 90, 59, 10, 94, 32, 44, 3, 89, 30, 27, 79, 46, 96, 27, 32, 18, 21, 92, 69, 81, 40, 40, 34, 68, 78, 24, 87, 42, 69, 23, 41, 78, 22, 6, 90, 99, 89, 50, 30, 20, 1, 43, 3, 70, 95, 33, 46, 44, 9, 69, 48, 33, 60, 65, 16, 82, 67, 61, 32, 21, 79, 75, 75, 13, 87, 70, 33 };
+    vector<int> arr = { 73,67,38,33 };
     cout << "Entrance ";
     for (auto& a : arr) cout << a << "  ";
     cout << endl;
 
     cout << "Result: "  << endl;
-    for (auto& s : countingSort(arr)) cout << s << "  ";
+    for (auto& s : gradingStudents(arr)) cout << s << "  ";
     cout << endl;
 }
